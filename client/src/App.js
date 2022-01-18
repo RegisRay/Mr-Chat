3faebd62f3537283f8bdfa069a0333bd847c7015
Crispin R <crispinr003@gmail.com>
2022-01-18 22:02:30 +0530

    Add roomKey attribute

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import Chat from "./Chat";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function App() {
-    const [popUp, setPopUp]=useState(false);
   return (
     <>
-        <Chat/>
+      <Chat roomKey="oki" />
     </>
   );
 }
